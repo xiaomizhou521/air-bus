6f628df612dc2c5b81d46d7e81b532e0485f5286
xiaoxiao12091010@126.com <xiaoxiao12091010@126.com>
2019-07-01 20:24:31 +0800

    test

--- src/main/java/com/main/data_show/DataShowApplication.java
+++ src/main/java/com/main/data_show/DataShowApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DataShowApplication {
 
     public static void main(String[] args) {
+        System.out.println(12345);
         SpringApplication.run(DataShowApplication.class, args);
     }
 
