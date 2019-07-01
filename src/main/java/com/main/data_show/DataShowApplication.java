3510440b87a4d13624695301fce11f6dae8c43c5
xiaoxiao12091010@126.com <xiaoxiao12091010@126.com>
2019-07-01 20:18:19 +0800

    initial commit

---
+++ src/main/java/com/main/data_show/DataShowApplication.java
@@ -0,0 +1,13 @@
+package com.main.data_show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataShowApplication.class, args);
+    }
+
+}
