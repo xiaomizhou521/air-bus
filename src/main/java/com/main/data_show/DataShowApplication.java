e61505a23027984f7cd9b267a12c09069ca130b0
xiaoxiao12091010@126.com <xiaoxiao12091010@126.com>
2019-07-02 21:21:31 +0800

    add  utility

--- src/main/java/com/main/data_show/DataShowApplication.java
+++ src/main/java/com/main/data_show/DataShowApplication.java
@@ -1,5 +1,6 @@
 package com.main.data_show;
 
+import cn.com.enorth.utility.Beans;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
